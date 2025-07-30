adac52d715371659d7245e718da945c8cfa9217c
BauveR <rick.bauve@gmail.com>
2025-07-30 11:10:50 +0200

    interfaces  customer info to keep the client info,  interface bidget to keep the savig budget, budget context type to keep the main budget,  and also the budget record to reamin the bugets  they will be uses for the hooks and also for the budget summary functions  and service card

--- src/types/types.ts
+++ src/types/types.ts
@@ -16,3 +16,48 @@ export interface HeaderOptions {
     pages?: number;
     languages?: number;
   }
+  export interface CustomerInfo {
+    name: string;
+    email: string;
+    phone: string;
+  }
+  
+  export interface Budget {
+    id: string;
+    customerInfo: CustomerInfo;
+    services: SelectedService[];
+    total: number;
+    discountedTotal: number | null;
+    date: Date;
+  }
+
+  export interface BudgetContextType {
+    selectedServices: SelectedService[];
+    toggleService: (service: ServiceOptions) => void;
+    updateServiceDetails: (code: string, details: { pages?: number; languages?: number }) => void;
+  
+    total: number;
+    discountedTotal: number | null;
+    applyAnnualDiscount: () => number;
+    resetDiscount: () => void;
+  
+    saveBudget: (customerInfo: CustomerInfo) => void;
+    ordenarPorNombre: () => void;
+    orderByDate: () => void;
+    filterBudgets: (term: string) => void;
+  
+    savedBudgets: Budget[];
+    savedFilteredBudgets: Budget[];
+    orderedBy: string; // 'NAME' | 'DATE'
+  }
+  
+  export interface BudgetRecord {
+    customerInfo: CustomerInfo;
+    services: SelectedService[];
+    total: number;
+    discountedTotal: number | null;
+    date: Date;
+  }
+  
+  
+  
